6ae581adfef242ca57df9fa2a9416f664dff5f26
hanhyochan <hcartist569@gmail.com>
2024-11-01 11:05:00 +0900

    11/1 할일 수정 간단 스타일링 적용 완료

--- version2.1.1/src/pages/TodoList/TodoItemEditTemplate.jsx
+++ version2.1.1/src/pages/TodoList/TodoItemEditTemplate.jsx
@@ -1,15 +1,17 @@
 import React, { useRef } from 'react';
 import { useView } from '../../context/viewContext';
 
-const TodoItemEditTemplate = ({ todo, handleEditTodos}) => {
+const TodoItemEditTemplate = ({ todo, handleEditTodos }) => {
     const TodoEditInput = useRef(null)
     const { setView } = useView()
     return (
-        <>
-            <input type='text' defaultValue={todo.content} ref={TodoEditInput} />
-            <button onClick={() => handleEditTodos(todo.id, TodoEditInput)}>저장</button>
-            <button onClick={() => setView(true)}>취소</button>
-        </>
+        <div className='flex items-center justify-between'>
+            <input type='text' defaultValue={todo.content} ref={TodoEditInput} className='w-[330px] p-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-[15px]' />
+            <div className='mt-[13px]'>
+                <button onClick={() => handleEditTodos(todo.id, TodoEditInput)} className='px-2 py-2 font-semibold text-white bg-orange-400 rounded-md shadow hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-opacity-50'>저장</button>
+                <button onClick={() => setView(true)} className='px-2 py-2 font-semibold text-white bg-gray-400 rounded-md shadow hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-opacity-50 ml-[10px]'>취소</button>
+            </div>
+        </div>
     )
 }
 
